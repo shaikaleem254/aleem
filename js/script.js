df82ecf55c5f2c2f1a505a648350306400b6d8cc
shaik aleem <134679641+shaikaleem254@users.noreply.github.com>
2023-11-13 20:41:50 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var billAmount=document.getElementById("bill-amount");
+var percAmount=document.getElementById('perc');
+var tipAmount=document.getElementById("tip");
+var totalAmount=document.getElementById("total-amount");
+function calculate()
+{
+    var result=percAmount.value/billAmount.value*100;
+    // alert(result);
+    tipAmount=document.getElementById('tip').;
+    // alert(billAmount.value);
+    alert(tipAmount)
+}
